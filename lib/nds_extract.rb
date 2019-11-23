4330b360f0c55d48ddd858d2271db34e43515375
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-23 19:35:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,4 +13,7 @@ def directors_totals(nds)
     while movie_index < nds[director_index][:movies].length do 
       result[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1 
+    end 
+    director_index += 1 
+  end   
 end
